4ec83dc974abe7f79f7fa5bcf5eaad0ad008007b
oracle <oracle@srv1.localdomain>
2025-12-04 01:59:54 +0400

    directory about cdb management

---
+++ basic_admin/check_ispdb_mod.sql
@@ -0,0 +1,45 @@
+
+/*Verify that the parameter DB_RECOVERY_FILE_DEST_SIZE cannot be changed in the PDB level. The ISPDB_MODIFIABLE is FALSE which means it cannot be modified at the PDB level. Observe that the values of the NAME column in V$SYSTEM_PARAMETER are in lower-case.
+*/
+
+SHOW CON_ID;
+
+col value format a15
+SELECT VALUE, VALUE/1024/1024/1024 GB, ISPDB_MODIFIABLE
+FROM V$SYSTEM_PARAMETER
+WHERE NAME='db_recovery_file_dest_size'; 
+
+SHOW PARAMETER DB_RECOVERY_FILE_DEST_SIZE;
+
+/* move the current containe from the root to a pdb
+You should receive the following error:
+ORA-65040: operation not allowed from within a pluggable database */
+
+ALTER SESSION SET CONTAINER=PDB1;
+
+SHOW CON_ID;
+
+SELECT VALUE FROM V$SYSTEM_PARAMETER WHERE NAME='db_recovery_file_dest_size';
+ALTER SYSTEM SET DB_RECOVERY_FILE_DEST_SIZE = 10903094248;
+
+
+--Change the current container to the root, and try to change the parameter value.
+--The parameter value could successfully be changed at the CDB level.
+
+ALTER SESSION SET CONTAINER=CDB$ROOT;
+SHOW CON_ID;
+
+ALTER SYSTEM SET DB_RECOVERY_FILE_DEST_SIZE = 10903094248 SCOPE=BOTH;
+SHOW PARAMETER DB_RECOVERY_FILE_DEST_SIZE;
+
+
+--Change the current container to PDB1 and query the value of the parameter.
+--The change made at the CDB level is seen by the PDB.
+ALTER SESSION SET CONTAINER=PDB1;
+SHOW_CON_ID;
+
+SELECT VALUE, VALUE/1024/1024/1024 GB FROM V$SYSTEM_PARAMETER WHERE
+NAME='db_recovery_file_dest_size';
+
+
+ALTER SESSION SET CONTAINER=CDB$ROOT; 
---
+++ basic_admin/modify_pdb_par.sql
@@ -0,0 +1,26 @@
+/*Verify that the parameter DDL_LOCK_TIMEOUT can be changed in the PDB level.
+The ISPDB_MODIFIABLE is TRUE, which means the parameter can be modified at the PDB level.*/
+
+show con_id;
+
+col VALUE for a10
+SELECT VALUE, ISPDB_MODIFIABLE
+FROM V$SYSTEM_PARAMETER
+WHERE NAME='ddl_lock_timeout'; 
+
+
+--Change the current container to PDB1 and try to change the parameter value.
+--Changing this parameter at the PDB level succeeds
+show con_id;
+ALTER SESSION SET CONTAINER=PDB1;
+ALTER SYSTEM SET DDL_LOCK_TIMEOUT = 12;
+
+--Set the current container back to the root and query information about the parameter.
+--The parameter has different value for each PDB.
+
+ALTER SESSION SET CONTAINER=CDB$ROOT;
+show con_id
+col name format a20
+SELECT CON_ID, NAME, VALUE
+ FROM V$SYSTEM_PARAMETER
+ WHERE NAME='ddl_lock_timeout';
