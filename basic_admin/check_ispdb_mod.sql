5241f9d29cf25af0590635c820d53dd41dfa2c79
oracle <oracle@srv1.localdomain>
2025-12-06 02:07:43 +0400

    cdb_task script added

--- basic_admin/check_ispdb_mod.sql
+++ basic_admin/check_ispdb_mod.sql
@@ -9,9 +9,10 @@ SELECT VALUE, VALUE/1024/1024/1024 GB, ISPDB_MODIFIABLE
 FROM V$SYSTEM_PARAMETER
 WHERE NAME='db_recovery_file_dest_size'; 
 
+
 SHOW PARAMETER DB_RECOVERY_FILE_DEST_SIZE;
 
-/* move the current containe from the root to a pdb
+/* move the current container from the root to a pdb
 You should receive the following error:
 ORA-65040: operation not allowed from within a pluggable database */
 
@@ -20,6 +21,7 @@ ALTER SESSION SET CONTAINER=PDB1;
 SHOW CON_ID;
 
 SELECT VALUE FROM V$SYSTEM_PARAMETER WHERE NAME='db_recovery_file_dest_size';
+
 ALTER SYSTEM SET DB_RECOVERY_FILE_DEST_SIZE = 10903094248;
 
 
@@ -30,11 +32,13 @@ ALTER SESSION SET CONTAINER=CDB$ROOT;
 SHOW CON_ID;
 
 ALTER SYSTEM SET DB_RECOVERY_FILE_DEST_SIZE = 10903094248 SCOPE=BOTH;
+
 SHOW PARAMETER DB_RECOVERY_FILE_DEST_SIZE;
 
 
 --Change the current container to PDB1 and query the value of the parameter.
 --The change made at the CDB level is seen by the PDB.
+
 ALTER SESSION SET CONTAINER=PDB1;
 SHOW_CON_ID;
 
@@ -43,3 +47,8 @@ NAME='db_recovery_file_dest_size';
 
 
 ALTER SESSION SET CONTAINER=CDB$ROOT; 
+
+
+
+
+
--- basic_admin/modify_pdb_par.sql
+++ basic_admin/modify_pdb_par.sql
@@ -11,15 +11,23 @@ WHERE NAME='ddl_lock_timeout';
 
 --Change the current container to PDB1 and try to change the parameter value.
 --Changing this parameter at the PDB level succeeds
-show con_id;
+
+
+show con_name;
+
 ALTER SESSION SET CONTAINER=PDB1;
+
+show con_name;
+
 ALTER SYSTEM SET DDL_LOCK_TIMEOUT = 12;
 
 --Set the current container back to the root and query information about the parameter.
 --The parameter has different value for each PDB.
 
 ALTER SESSION SET CONTAINER=CDB$ROOT;
-show con_id
+
+show con_name
+
 col name format a20
 SELECT CON_ID, NAME, VALUE
  FROM V$SYSTEM_PARAMETER
